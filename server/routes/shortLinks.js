fc82d32fc15d546e2e93f649647c229dc75178e3
Akshad Gawde <gawdeakshad.com>
2026-03-17 18:23:04 +0530

    pdf

---
+++ server/routes/shortLinks.js
@@ -0,0 +1,14 @@
+/**
+ * SHORT LINKS FEATURE - DEPRECATED
+ * This file is no longer used. PDFs are accessed directly via pdfUrl from MongoDB.
+ * All routes have been disabled and removed.
+ */
+
+const express = require("express");
+const router = express.Router();
+
+// This route is no longer used - short links feature has been removed
+// PDFs are now accessed directly via the pdfUrl field from the database
+
+module.exports = router;
+
